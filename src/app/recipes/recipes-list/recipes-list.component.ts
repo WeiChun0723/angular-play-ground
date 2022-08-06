ccb56b60a26963f966897f6b73d9d11479a5b31c
WeiChun0723 <laiwc0723@gmail.com>
2022-08-06 15:43:06 +0800

    Build fundamental for the project

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipes-list',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
+  recipes: Recipe[] = [new Recipe("Kam heong", "Best", "https://i.ytimg.com/vi/Ydvoe6WK6K0/hqdefault.jpg")]
 
   constructor() { }
 
